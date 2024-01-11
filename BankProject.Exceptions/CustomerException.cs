3474fdefac53c40f2c16d40eddd733940d0da050
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 19:41:33 +0530

    Created a Custom Exception class for handling exceptions related to Customer in Exceptions Project

---
+++ BankProject.Exceptions/CustomerException.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Exceptions
+{
+    public class CustomerException : ApplicationException
+    {
+        public CustomerException():base()
+        { 
+        }
+
+        public CustomerException(string message) : base(message) 
+        {
+        }
+
+        public CustomerException(string message,  Exception innerException) : base(message, innerException)
+        { 
+        }
+    }
+}
