d3da4a32644ecf99463d8152fc51c2e5cd7afd85
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 22:12:02 +0530

    Written actual logic for AddCustomer() in CustomersService

--- BankProject.Services/CustomersService.cs
+++ BankProject.Services/CustomersService.cs
@@ -1,7 +1,9 @@
-﻿using BankProject.Repositories;
+﻿using BankProject.Entities;
+using BankProject.Repositories;
 using BankProject.RepositoryContracts;
 using BankProject.ServiceContracts;
 using BankProject.ServiceContracts.Dto;
+using BankProject.Services.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +22,26 @@ namespace BankProject.Services
         }
         public CustomerResponse AddCustomer(CustomerAddRequest? customerAddRequest)
         {
-            throw new NotImplementedException();
+            //1. check for null condition for customeraddrequest
+            if(customerAddRequest == null)
+            { 
+                throw new ArgumentNullException(nameof(customerAddRequest)); 
+            }
+
+            //2. validate all properties of customeraddrequest
+            ValidationHelper.ModelValidation(customerAddRequest);
+
+            //3. convert customeraddrequest to customer type
+            Customer customer = customerAddRequest.ToCustomer();
+
+            //4. create new customerid
+            customer.CustomerID = Guid.NewGuid();
+
+            //5. Then invoke corresponding repository
+            _customersRepository.AddCustomer(customer);
+
+            //6. return customerresponse object with newly generated customerid
+            return customer.ToCustomerResponse();
         }
 
         public bool DeleteCustomer(Guid? customerID)
