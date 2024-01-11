8c1a48dfc0e1fcb1a1725e6a655e3e91383e7389
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 19:42:21 +0530

    Created a validation helper class for validating properties during model binding

---
+++ BankProject.Entities/Helpers/ValidationHelper.cs
@@ -0,0 +1,22 @@
+﻿using BankProject.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Entities.Helpers
+{
+    public class ValidationHelper
+    {
+        internal static void ModelValidation(object obj)
+        {
+            ValidationContext validationContext = new ValidationContext(obj);
+            List<ValidationResult> validationResults = new List<ValidationResult>();
+            bool isValid = Validator.TryValidateObject(obj, validationContext, validationResults, true);
+            if (!isValid)
+                throw new CustomerException(validationResults.FirstOrDefault()?.ErrorMessage);
+        }
+    }
+}
