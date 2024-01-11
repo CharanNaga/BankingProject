d8d919ea0e55623e22594e33c7b02797aa185fe3
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 17:50:49 +0530

    Modified Program.cs to include Main Menu logic for the successful credentials

--- BankProject.Presentation/Program.cs
+++ BankProject.Presentation/Program.cs
@@ -21,12 +21,45 @@
 
             if (userName == "Admin" && password == "Admin123")
             {
-                Console.WriteLine("______________________MAIN MENU__________________________________________");
+                int mainMenuChoice = -1;
 
+                do
+                {
+                    Console.WriteLine("______________________MAIN MENU__________________________________________");
+                    Console.WriteLine("1. Customers");
+                    Console.WriteLine("2. Accounts");
+                    Console.WriteLine("3. Funds Transfer");
+                    Console.WriteLine("4. Funds Transfer Statement");
+                    Console.WriteLine("5. Account Statement");
+                    Console.WriteLine("0. Exit");
+
+                    Console.Write("Enter your choice [0-5]: ");
+                    mainMenuChoice = Convert.ToInt32(Console.ReadLine());
+
+                    switch (mainMenuChoice)
+                    {
+                        case 1: //TO DO: Display customers menu
+                            break;
+                        case 2: //TO DO: Display accounts menu
+                            break;
+                        case 3: //TO DO: Display funds transfer menu
+                            break;
+                        case 4: //TO DO: Display funds transfer statement menu
+                            break;
+                        case 5: //TO DO: Display account statement menu
+                            break;
+                        case 0:
+                            break;
+                    }
+
+                } while (mainMenuChoice != 0);
+            }
+
+            else
+            {
+                Console.WriteLine("Invalid UserName or Password");
             }
         }
-
-        Console.WriteLine("Thanks!! Exiting");
-        
+        Console.WriteLine("Thanks!! Exiting the application");
     }
 }
