81885634014413e4dd63e1825e57d12ef486f88d
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 19:11:10 +0530

    Created a Customer model class in Entities Project

---
+++ BankProject.Entities/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+
+namespace BankProject.Entities
+{
+    public class Customer
+    {
+        public Guid CustomerID { get; set; }
+        public long CustomerCode { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public string? Landmark { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? Mobile { get; set; }
+    }
+}
