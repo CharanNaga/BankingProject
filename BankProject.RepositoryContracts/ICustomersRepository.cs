1adb671a11c9103f0b4e4e86aa4c02356dbb149b
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 19:54:53 +0530

    Create an interface ICustomersRepository.cs and defined some set of methods to perform CRUD operations

---
+++ BankProject.RepositoryContracts/ICustomersRepository.cs
@@ -0,0 +1,22 @@
+﻿using BankProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.RepositoryContracts
+{
+    public interface ICustomersRepository
+    {
+        List<Customer> GetCustomers();
+
+        List<Customer> GetFilteredCustomers(Predicate<Customer> condition);
+
+        Customer AddCustomer(Customer customer);
+
+        Customer UpdateCustomer(Customer customer);
+
+        bool DeleteCustomer(Guid customerID);
+    }
+}
