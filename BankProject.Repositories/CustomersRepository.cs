8f7676a853a3e57205fd11f5836b7825520d2c9b
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 19:56:42 +0530

    Created a new repository class CustomersRepository implementing all the methods with dummy logic

---
+++ BankProject.Repositories/CustomersRepository.cs
@@ -0,0 +1,38 @@
+﻿using BankProject.Entities;
+using BankProject.RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Repositories
+{
+    public class CustomersRepository : ICustomersRepository
+    {
+        public Customer AddCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteCustomer(Guid customerID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Customer> GetFilteredCustomers(Predicate<Customer> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Customer UpdateCustomer(Customer customer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
