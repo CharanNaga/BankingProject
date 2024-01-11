9599dc739e884c5d84c69de932f7233d412b892a
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 17:42:19 +0530

    Modified Program.cs to have initial logic for login form

--- BankProject.Presentation/Program.cs
+++ BankProject.Presentation/Program.cs
@@ -2,6 +2,31 @@
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Console.WriteLine("*****************************************************************************");
+        Console.WriteLine("                     BANKING APPLICATION                                     ");
+        Console.WriteLine("*****************************************************************************");
+        Console.WriteLine("\n\n---------------------LOGIN PAGE---------------------------------------------");
+
+        //declaring userName & password
+        string userName;
+        string password;
+
+        Console.Write("Enter User Name: ");
+        userName = Console.ReadLine();
+
+        if(!string.IsNullOrEmpty(userName))
+        {
+            Console.Write("Enter Password: ");
+            password = Console.ReadLine();
+
+            if (userName == "Admin" && password == "Admin123")
+            {
+                Console.WriteLine("______________________MAIN MENU__________________________________________");
+
+            }
+        }
+
+        Console.WriteLine("Thanks!! Exiting");
+        
     }
 }
