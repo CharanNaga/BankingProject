c8d62ee560210941ad8473a7846a92ee0faf353d
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 21:57:23 +0530

    Created Dto under ServiceContracts project for handling request and response objects

---
+++ BankProject.ServiceContracts/Dto/CustomerResponse.cs
@@ -0,0 +1,65 @@
+﻿using BankProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.ServiceContracts.Dto
+{
+    public class CustomerResponse
+    {
+        public Guid CustomerID { get; set; }
+        public long CustomerCode { get; set; }
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public string? Landmark { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+        public string? Mobile { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if(obj == null) return false;
+            if(obj.GetType() != typeof(CustomerResponse)) return false;
+            CustomerResponse customerResponse = (CustomerResponse)obj;
+
+            return CustomerID == customerResponse.CustomerID &&
+                CustomerCode == customerResponse.CustomerCode &&
+                CustomerName == customerResponse.CustomerName &&
+                Address == customerResponse.Address &&
+                Landmark == customerResponse.Landmark &&
+                City == customerResponse.City &&
+                Country == customerResponse.Country &&
+                Mobile == customerResponse.Mobile;
+        }
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return $"CustomerName: {CustomerName}, \nAddress: {Address}, \nLandmark: {Landmark}, \nCity: {City}, \nCountry: {Country}, \nMobile: {Mobile}";
+        }
+    }
+
+    public static class CustomerExtensions
+    {
+        public static CustomerResponse ToCustomerResponse(this Customer customer)
+        {
+            return new CustomerResponse()
+            {
+                CustomerID = customer.CustomerID,
+                CustomerCode = customer.CustomerCode,
+                CustomerName = customer.CustomerName,
+                Address = customer.Address,
+                Landmark = customer.Landmark,
+                City = customer.City,
+                Country = customer.Country,
+                Mobile = customer.Mobile
+            };
+        }
+    }
+}
---
+++ BankProject.ServiceContracts/Dto/CustomerUpdateRequest.cs
@@ -0,0 +1,43 @@
+﻿using BankProject.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.ServiceContracts.Dto
+{
+    public class CustomerUpdateRequest
+    {
+        public Guid CustomerID { get; set; }
+
+        [Range(0, long.MaxValue, ErrorMessage = "Customer Code should be a positive value")]
+        public long CustomerCode { get; set; }
+
+        [Required(ErrorMessage = "Customer Name shouldn't be blank")]
+        [StringLength(40, ErrorMessage = "Customer Name consists maximum length of 40")]
+        public string? CustomerName { get; set; }
+        public string? Address { get; set; }
+        public string? Landmark { get; set; }
+        public string? City { get; set; }
+        public string? Country { get; set; }
+
+        [RegularExpression(@"^[0-9]{10}$", ErrorMessage = "Mobile Number consists of 10 digits only")]
+        public string? Mobile { get; set; }
+
+        public Customer ToCustomer()
+        {
+            return new Customer()
+            {
+                CustomerID = CustomerID,
+                CustomerCode = CustomerCode,
+                CustomerName = CustomerName,
+                Address = Address,
+                Landmark = Landmark,
+                City = City,
+                Country = Country
+            };
+        }
+    }
+}
