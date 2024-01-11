67677a04f0ca7f27e3a296ed8a59bcc48c5ea1be
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-11 21:59:26 +0530

    Created a new interface ICustomersService in ServiceContracts project & written few method definitions

---
+++ BankProject.ServiceContracts/ICustomersService.cs
@@ -0,0 +1,22 @@
+﻿using BankProject.ServiceContracts.Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.ServiceContracts
+{
+    public interface ICustomersService
+    {
+        CustomerResponse AddCustomer(CustomerAddRequest? customerAddRequest);
+
+        List<CustomerResponse> GetCustomers();
+
+        List<CustomerResponse> GetFilteredCustomers(string searchBy, string searchString);
+
+        CustomerResponse UpdateCustomer(CustomerUpdateRequest? customerUpdateRequest);
+
+        bool DeleteCustomer(Guid? customerID);
+    }
+}
