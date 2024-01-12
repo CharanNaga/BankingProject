48ef572f1cea624fd137cf48a45e35b9bbb7909f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-13 00:17:21 +0530

    Modified Program.cs to run HostBuilder(args), added required classes to Ioc & called all the display related static class code

--- BankProject.Presentation/Program.cs
+++ BankProject.Presentation/Program.cs
@@ -1,158 +1,26 @@
 ﻿using BankProject.Configuration;
 using BankProject.Presentation;
+using BankProject.Repositories;
+using BankProject.RepositoryContracts;
+using BankProject.ServiceContracts;
+using BankProject.Services;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 public class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("*****************************************************************************");
-        Console.WriteLine("                     BANKING APPLICATION                                     ");
-        Console.WriteLine("*****************************************************************************");
-        Console.WriteLine("\n\n---------------------LOGIN PAGE---------------------------------------------");
-
-        //declaring userName & password
-        string? userName = null, password = null;
-
-        while (true)
-        {
-            Console.Write("Enter User Name: ");
-            userName = Console.ReadLine();
-
-            if (!string.IsNullOrEmpty(userName))
-            {
-                Console.Write("Enter Password: ");
-                password = Console.ReadLine();
-            }
-            else
-            {
-                break;
-            }
-
-            int mainMenuChoice = -1;
-
-            if (userName == Settings.UserName && password == Settings.Password)
-            {
-
-                do
+        CreateHostBuilder(args).Build().Run();
+    }
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
                 {
-                    Console.WriteLine("______________________MAIN MENU__________________________________________");
-                    Console.WriteLine("1. Customers");
-                    Console.WriteLine("2. Accounts");
-                    Console.WriteLine("3. Funds Transfer");
-                    Console.WriteLine("4. Funds Transfer Statement");
-                    Console.WriteLine("5. Account Statement");
-                    Console.WriteLine("0. Exit");
+                    // Add your services here using dependency injection
+                    services.AddTransient<ICustomersRepository,CustomersRepository>();
+                    services.AddTransient<ICustomersService,CustomersService>();
 
-                    Console.Write("Enter your choice [0-5]: ");
-                    while (!int.TryParse(Console.ReadLine(), out mainMenuChoice) || (mainMenuChoice < 0 || mainMenuChoice > 5))
-                    {
-                        Console.Write("Invalid Choice.\nEnter your choice [0-5]: ");
-                    }
-
-                    switch (mainMenuChoice)
-                    {
-                        case 1:
-                            CustomersMenu();
-                            break;
-                        case 2:
-                            AccountsMenu();
-                            break;
-                        case 3: //TO DO: Display funds transfer menu
-                            break;
-                        case 4: //TO DO: Display funds transfer statement menu
-                            break;
-                        case 5: //TO DO: Display account statement menu
-                            break;
-                        case 0:
-                            break;
-                    }
-
-                } while (mainMenuChoice != 0);
-            }
-
-            else
-            {
-                Console.WriteLine("Invalid UserName or Password");
-            }
-            if (mainMenuChoice == 0)
-                break;
-        }
-
-        Console.WriteLine("Thanks for your time!! Exiting the application");
-    }
-
-    static void CustomersMenu()
-    {
-        int customerMenuChoice = -1;
-        do
-        {
-            Console.WriteLine("______________________CUSTOMER MENU__________________________________________");
-            Console.WriteLine("1. Add Customer");
-            Console.WriteLine("2. Update Customer");
-            Console.WriteLine("3. Fetch Customers");
-            Console.WriteLine("4. Filter Customers");
-            Console.WriteLine("5. Delete Customer");
-            Console.WriteLine("0. Back to Main Menu");
-
-            Console.Write("Enter your choice [0-5]: ");
-            while (!int.TryParse(Console.ReadLine(), out customerMenuChoice) || (customerMenuChoice < 0 || customerMenuChoice > 5))
-            {
-                Console.Write("Invalid Choice.\nEnter your choice [0-5]: ");
-            }
-
-            switch (customerMenuChoice)
-            {
-                case 1: CustomersPresentation.AddCustomer();
-                    break;
-                case 2: CustomersPresentation.UpdateCustomer();
-                    break;
-                case 3: CustomersPresentation.DisplayCustomers();
-                    break;
-                case 4: CustomersPresentation.FilteredCustomers();
-                    break;
-                case 5: CustomersPresentation.DeleteCustomer();
-                    break;
-                case 0:
-                    break;
-            }
-        } while (customerMenuChoice != 0);
-    }
-
-
-    static void AccountsMenu()
-    {
-        int accountMenuChoice = -1;
-        do
-        {
-            Console.WriteLine("______________________ACCOUNT MENU__________________________________________");
-            Console.WriteLine("1. Add Account");
-            Console.WriteLine("2. Update Account");
-            Console.WriteLine("3. Fetch Accounts");
-            Console.WriteLine("4. Filter Accounts");
-            Console.WriteLine("5. Delete Account");
-            Console.WriteLine("0. Back to Main Menu");
-
-            Console.Write("Enter your choice [0-5]: ");
-            while (!int.TryParse(Console.ReadLine(), out accountMenuChoice) || (accountMenuChoice < 0 || accountMenuChoice > 5))
-            {
-                Console.Write("Invalid Choice.\nEnter your choice [0-5]: ");
-            }
-
-            switch (accountMenuChoice)
-            {
-                case 1: //TO DO: Create Account
-                    break;
-                case 2: //TO DO: Update Account
-                    break;
-                case 3: //TO DO: Get all Accounts
-                    break;
-                case 4: //TO DO: Get Account by AccountID
-                    break;
-                case 5: //TO DO: Delete Account
-                    break;
-                case 0:
-                    break;
-            }
-        } while (accountMenuChoice != 0);
-    }
+                    MainMenuDisplay.DisplayMenu();
+                });
 }
