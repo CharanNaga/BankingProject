f3bdcfe0b294d5fd3d56b344c79da5e6d4d87398
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-13 01:08:02 +0530

    Modified Program.cs to create a scope and run the all the display related logic which is present in MainMenuDisplay class

--- BankProject.Presentation/Program.cs
+++ BankProject.Presentation/Program.cs
@@ -9,18 +9,28 @@ using Microsoft.Extensions.Hosting;
 
 public class Program
 {
-    static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
-        CreateHostBuilder(args).Build().Run();
+        var host = CreateHostBuilder(args).Build();
+
+        using (var scope = host.Services.CreateScope())
+        {
+            var serviceProvider = scope.ServiceProvider;
+            var mainMenuDisplay = serviceProvider.GetRequiredService<MainMenuDisplay>();
+            await mainMenuDisplay.RunAsync();
+        }
+
+        await host.RunAsync();
     }
-    public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
+    public static IHostBuilder CreateHostBuilder(string[] args) 
+        => Host.CreateDefaultBuilder(args)
                 .ConfigureServices((hostContext, services) =>
                 {
                     // Add your services here using dependency injection
                     services.AddTransient<ICustomersRepository,CustomersRepository>();
                     services.AddTransient<ICustomersService,CustomersService>();
-
-                    MainMenuDisplay.DisplayMenu();
+                    services.AddTransient<CustomersPresentation,CustomersPresentation>();
+                    services.AddTransient<CustomersMenuDisplay,CustomersMenuDisplay>();
+                    services.AddTransient<MainMenuDisplay,MainMenuDisplay>();
                 });
 }
