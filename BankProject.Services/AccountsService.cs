576b1a6f001e72f86fe89221b6ad341d44c35d2f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-16 21:44:24 +0530

    Created a new service for implementing IAccountsService with dummy implementation

---
+++ BankProject.Services/AccountsService.cs
@@ -0,0 +1,34 @@
+﻿using BankProject.Entities;
+using BankProject.ServiceContracts;
+using BankProject.ServiceContracts.Dto;
+
+namespace BankProject.Services
+{
+    public class AccountsService : IAccountsService
+    {
+        public AccountResponse AddAccount(AccountAddRequest? accountAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAccount(Guid? accountID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AccountResponse> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<AccountResponse> GetFilteredAccounts(Predicate<Account> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public AccountResponse UpdateAccount(AccountUpdateRequest? accountUpdateRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
