39c553f754112231d7763761cad4025f1f0fbf69
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-16 20:53:00 +0530

    Created a new Repository implementing IAccountsRepository with dummy implementation

---
+++ BankProject.Repositories/AccountsRepository.cs
@@ -0,0 +1,38 @@
+﻿using BankProject.Entities;
+using BankProject.RepositoryContracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Repositories
+{
+    public class AccountsRepository : IAccountsRepository
+    {
+        public Account AddAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteAccount(Guid accountID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetAccounts()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Account> GetFilteredAccounts(Predicate<Account> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Account UpdateAccount(Account account)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
