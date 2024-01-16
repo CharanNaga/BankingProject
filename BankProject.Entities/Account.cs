eff34c187a8949a63607c28b542e2dcd0ad25088
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-16 20:27:16 +0530

    Created an entity Account.cs for handling Accounts in Entities Project

---
+++ BankProject.Entities/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankProject.Entities
+{
+    public class Account
+    {
+        public Guid CustomerID { get; set; }
+        public Guid AccountID { get; set; }
+        [Range(0,long.MaxValue,ErrorMessage ="Account Number can't be negative")]
+        public long AccountNumber { get; set; }
+        [Range(0, long.MaxValue, ErrorMessage = "Balance can't be negative")]
+        public decimal Balaance { get; set; }
+    }
+}
