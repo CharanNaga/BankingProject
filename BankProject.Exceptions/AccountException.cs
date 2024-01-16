1a1b786b5c56f3cf51fe36f409d9a153f77ec58d
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-16 20:36:21 +0530

    Created a new exception class for handling exceptions related to Accounts entity

---
+++ BankProject.Exceptions/AccountException.cs
@@ -0,0 +1,15 @@
+﻿namespace BankProject.Exceptions
+{
+    public class AccountException : ApplicationException
+    {
+        public AccountException() : base()
+        { 
+        }
+        public AccountException(string message) : base(message)
+        {
+        }
+        public AccountException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+}
