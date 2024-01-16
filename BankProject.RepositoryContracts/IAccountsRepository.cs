dd4e1cbf5a7bdb37066d33ed9ae813e955f89211
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-16 20:43:31 +0530

    Created a new repositorycontract for handling CRUD operations related to Account entity

---
+++ BankProject.RepositoryContracts/IAccountsRepository.cs
@@ -0,0 +1,17 @@
+﻿using BankProject.Entities;
+
+namespace BankProject.RepositoryContracts
+{
+    public interface IAccountsRepository
+    {
+        List<Account> GetAccounts();
+
+        List<Account> GetFilteredAccounts(Predicate<Account> condition);
+
+        Account AddAccount(Account account);
+
+        Account UpdateAccount(Account account);
+
+        bool DeleteAccount(Guid accountID);
+    }
+}
