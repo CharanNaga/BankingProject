5683986370a9ee57509683331784af2341a5151f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-16 21:43:47 +0530

    Created a service contract for handling business logic related to Account entity

---
+++ BankProject.ServiceContracts/IAccountsService.cs
@@ -0,0 +1,18 @@
+﻿using BankProject.Entities;
+using BankProject.ServiceContracts.Dto;
+
+namespace BankProject.ServiceContracts
+{
+    public interface IAccountsService
+    {
+        AccountResponse AddAccount(AccountAddRequest? accountAddRequest);
+
+        List<AccountResponse> GetAccounts();
+
+        List<AccountResponse> GetFilteredAccounts(Predicate<Account> condition);
+
+        AccountResponse UpdateAccount(AccountUpdateRequest? accountUpdateRequest);
+
+        bool DeleteAccount(Guid? accountID);
+    }
+}
