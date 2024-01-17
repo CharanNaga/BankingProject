d9dd4d8067b64434547b3d127cc4cf88d6e86116
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-17 15:20:51 +0530

    Created a new entity Transaction.cs

---
+++ BankProject.Entities/Transaction.cs
@@ -0,0 +1,11 @@
+﻿namespace BankProject.Entities
+{
+    public class Transaction
+    {
+        public Guid TransactionID { get; set; }
+        public Guid SourceAccountID { get; set; }
+        public Guid DestinationAccountID { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime TransactionDateTime { get; set; }
+    }
+}
