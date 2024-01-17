ccfa6f839c349a51f65288cb0fe3b924a0dd40a9
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-17 15:31:48 +0530

    Created a new repository contract for handling crud operations related to Transactions entity

---
+++ BankProject.RepositoryContracts/ITransactionsRepository.cs
@@ -0,0 +1,17 @@
+﻿using BankProject.Entities;
+
+namespace BankProject.RepositoryContracts
+{
+    public interface ITransactionsRepository
+    {
+        List<Transaction> GetTransactions();
+
+        List<Transaction> GetFilteredTransactions(Predicate<Transaction> condition);
+
+        Transaction AddTransaction(Transaction transaction);
+
+        Transaction UpdateTransaction(Transaction transaction);
+
+        bool DeleteTransaction(Guid transactionID);
+    }
+}
