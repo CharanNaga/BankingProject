3f185f252e5f0fd3ebfc23b6eddeb64cf2de45da
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-22 12:22:58 +0530

    Modified TransactionsRepository by implementing AddTransaction() with actual logic

--- BankProject.Repositories/TransactionsRepository.cs
+++ BankProject.Repositories/TransactionsRepository.cs
@@ -1,13 +1,35 @@
 ﻿using BankProject.Entities;
+using BankProject.Exceptions;
 using BankProject.RepositoryContracts;
+using System.Security.Principal;
 
 namespace BankProject.Repositories
 {
     public class TransactionsRepository : ITransactionsRepository
     {
+        private readonly List<Transaction> _transactions;
+
+        public TransactionsRepository(List<Transaction> transactions)
+        {
+            _transactions = transactions;
+        }
+
+
         public Transaction AddTransaction(Transaction transaction)
         {
-            throw new NotImplementedException();
+            try
+            {
+                _transactions.Add(transaction);
+                return transaction;
+            }
+            catch (TransactionException)
+            {
+                throw;
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
 
         public bool DeleteTransaction(Guid transactionID)
