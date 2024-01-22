62473d1bf031f6ecee18189d527cfd3c77f94e2f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-22 13:57:09 +0530

    Created a service contract for handling business logic related to transactions entity

---
+++ BankProject.ServiceContracts/ITransactionsService.cs
@@ -0,0 +1,14 @@
+﻿using BankProject.Entities;
+using BankProject.ServiceContracts.Dto;
+
+namespace BankProject.ServiceContracts
+{
+    public interface ITransactionsService
+    {
+        TransactionResponse AddTransaction(TransactionAddRequest? transactionAddRequest);
+        List<TransactionResponse> GetTransactions();
+        List<TransactionResponse> GetFilteredTransactions(Predicate<Transaction> predicate);
+        TransactionResponse UpdateTransaction(TransactionUpdateRequest? transactionUpdateRequest);
+        bool DeleteTransaction(Guid transactionID);
+    }
+}
