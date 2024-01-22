f61c9ec9a3ce1ed57b4a30c9af8b72a071e4c602
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-22 13:59:03 +0530

    Created a Service class 'TransactionsService' & implemented respective contract with dummy logic

---
+++ BankProject.Services/TransactionsService.cs
@@ -0,0 +1,34 @@
+﻿using BankProject.Entities;
+using BankProject.ServiceContracts;
+using BankProject.ServiceContracts.Dto;
+
+namespace BankProject.Services
+{
+    public class TransactionsService : ITransactionsService
+    {
+        public TransactionResponse AddTransaction(TransactionAddRequest? transactionAddRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteTransaction(Guid transactionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TransactionResponse> GetFilteredTransactions(Predicate<Transaction> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TransactionResponse> GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TransactionResponse UpdateTransaction(TransactionUpdateRequest? transactionUpdateRequest)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
