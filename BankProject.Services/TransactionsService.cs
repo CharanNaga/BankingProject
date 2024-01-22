bbbc6d1e5c1983c818865d6aad2550b54980937f
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-22 14:17:19 +0530

    Modified TransactionsService by implementing AddTransaction() with actual logic

--- BankProject.Services/TransactionsService.cs
+++ BankProject.Services/TransactionsService.cs
@@ -1,14 +1,74 @@
 ﻿using BankProject.Entities;
+using BankProject.Exceptions;
+using BankProject.RepositoryContracts;
 using BankProject.ServiceContracts;
 using BankProject.ServiceContracts.Dto;
+using BankProject.Services.Helpers;
 
 namespace BankProject.Services
 {
     public class TransactionsService : ITransactionsService
     {
+        private readonly ITransactionsRepository _transactionsRepository;
+        private readonly IAccountsRepository _accountsRepository;
+
+        public TransactionsService(ITransactionsRepository transactionsRepository, IAccountsRepository accountsRepository)
+        {
+            _transactionsRepository = transactionsRepository;
+            _accountsRepository = accountsRepository;
+        }
+
         public TransactionResponse AddTransaction(TransactionAddRequest? transactionAddRequest)
         {
-            throw new NotImplementedException();
+            try
+            {
+                //1. check for null condition for transactionAddRequest
+                if(transactionAddRequest == null)
+                    throw new TransactionException(nameof(transactionAddRequest));
+
+                //2. validate all properties of transactionAddRequest
+                ValidationHelper.ModelValidation(transactionAddRequest);
+
+                //3. convert transactionAddRequest to Transaction type
+                Transaction transaction = transactionAddRequest.ToTransaction();
+
+                //4. generate new transactionID
+                transaction.TransactionID = Guid.NewGuid();
+
+                //5. performing logic by retrieving the accounts
+                var sourceAccount = _accountsRepository.GetFilteredAccounts(temp => temp.AccountID == transaction.SourceAccountID).FirstOrDefault();
+                var destinationAccount = _accountsRepository.GetFilteredAccounts(temp => temp.AccountID == transaction.DestinationAccountID).FirstOrDefault();
+
+                if(sourceAccount != null && destinationAccount != null)
+                {
+                    if (sourceAccount.Balance < transaction.Amount)
+                    {
+                        throw new TransactionException($"Source account has insuffient funds for transaction of {transaction.Amount}");
+                    }
+
+                    sourceAccount.Balance -= transaction.Amount;
+                    destinationAccount.Balance += transaction.Amount;
+
+                    //6. invoke corresponding repository
+                    _transactionsRepository.AddTransaction(transaction);
+
+                    _accountsRepository.UpdateAccount(sourceAccount);
+                    _accountsRepository.UpdateAccount(destinationAccount);
+
+                    //7. return transactionresponse object
+                    return transaction.ToTransactionResponse();
+                }
+                throw new TransactionException("Source account or destination account number is invalid");
+
+            }
+            catch(TransactionException)
+            {
+                throw;
+            }
+            catch(Exception)
+            {
+                throw;
+            }
         }
 
         public bool DeleteTransaction(Guid transactionID)
