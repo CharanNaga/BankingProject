c280648def1af7debb39206fca38e4a958bc548a
CharanNaga <nagasaicharansinghbondili@gmail.com>
2024-01-22 12:18:44 +0530

    Created TransactionsRepository & implemented the corresponding contract using dummy implementation

---
+++ BankProject.Repositories/TransactionsRepository.cs
@@ -0,0 +1,33 @@
+﻿using BankProject.Entities;
+using BankProject.RepositoryContracts;
+
+namespace BankProject.Repositories
+{
+    public class TransactionsRepository : ITransactionsRepository
+    {
+        public Transaction AddTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteTransaction(Guid transactionID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> GetFilteredTransactions(Predicate<Transaction> condition)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Transaction> GetTransactions()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Transaction UpdateTransaction(Transaction transaction)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
